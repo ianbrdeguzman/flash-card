a4bf629c3d64b0599533ba6536601ec7c4217ea4
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-12-19 01:56:35 -0500

    installed react query firebase to do client mutations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { Nunito } from '@next/font/google';
+import { Navigation } from '../components/Navigation';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
-import Link from 'next/link';
 
 const nunito = Nunito({ subsets: ['latin'] });
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,19 +19,12 @@ export default function App({ Component, pageProps }: AppProps) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"
         ></link>
       </Head>
-      <main className={nunito.className}>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/create">Create</Link>
-            </li>
-          </ul>
-        </nav>
-        <Component {...pageProps} />
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <main className={nunito.className}>
+          <Navigation />
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
     </>
   );
 }
